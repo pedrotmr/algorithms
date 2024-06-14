6db60fa2181853ae58151650b823c0daea037144
Pedro Ramos <pedrotr@hotmail.com>
2024-06-14 01:40:51 -0300

    binary search

---
+++ src/day1/BinarySearchList.ts
@@ -0,0 +1,19 @@
+// ONLY WORKS ON SORTED ARRAYS
+
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let left = 0;
+    let right = haystack.length - 1;
+    
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
+        if (haystack[mid] === needle) return true
+
+        if (haystack[mid] < needle) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return false
+}
